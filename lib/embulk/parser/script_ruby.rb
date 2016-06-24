d0ad198f6d6ae18c726d359df0fd8d8442aec874
shinjiikeda <gm.ikeda@gmail.com>
2016-06-24 16:11:59 +0900

    update

--- lib/embulk/parser/script_ruby.rb
+++ lib/embulk/parser/script_ruby.rb
@@ -9,28 +9,24 @@ module Embulk
         task = {
           "script" => config.param("script", :string),
           "class" => config.param("class", :string),
-          "columns" => config.param("columns", :string), 
+          "columns" => config.param("columns", :array), 
         }
 
         c = 0
-        out_columns = task['columns'].map do | e |
+        columns = task['columns'].map do | e |
           col = Column.new(c, e['name'], e['type'].to_sym)
           c+=1
           col
         end
 
-        yield(task, out_columns)
+        yield(task, columns)
       end
 
       def init
         # initialization code:
         @script = task['script']
-
-        @out_map = {}
-        out_schema.each do | e |
-          @out_map[e['name']] = true
-        end
-
+        @columns = task['columns']
+        
         require @script
         @parser_class = Object.const_get(task['class']).new()
       end
@@ -39,7 +35,7 @@ module Embulk
         while file = file_input.next_file
           @parser_class.parser(file) do |record|
             out_record = []
-            out_schema.sort_by{|e| e['index']}.each do | e |
+            @columns.each do | e |
               out_record << record[e['name']] if record.has_key?(e['name'])
             end
             page_builder.add(out_record) if  out_record.size > 0
